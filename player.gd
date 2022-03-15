b4056d05e3d61bf9b60c29cf83786803e651d260
tcsbcoach <tcsbcoach@gmail.com>
2022-03-14 17:01:19 -0700

    Health, invincibility and begginings of blue

--- player.gd
+++ player.gd
@@ -11,19 +11,51 @@ onready var sprite = $AnimatedSprite
 onready var original
 onready var blue = false
 onready var health = 15 
+var label
+var isInvincible = false
+var invincibleTimer = 0
+var invincibleTimerConstant = 2
+var blueMeter = 5
+var blueMeterMax = 5
+var maxMeterWidth
+var meter
 
 func _ready():
 	# The player follows the mouse cursor automatically, so there's no point
 	# in displaying the mouse cursor.
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	original = sprite.modulate
+	label = get_node("/root/Shower/Label")
+	label.text = "Health:" + str(health)
+	meter = get_node("/root/Shower/Meter")
+	maxMeterWidth = meter.rect_size.x
 
+func _process(delta):
+	if isInvincible == true:
+		sprite.frame = 1
+		invincibleTimer -= delta
+		if invincibleTimer <= 0:
+			isInvincible = false
+	elif touching >= 1: #and blue == false:
+		sprite.frame = 1
+		health = health - 1
+		label.text = "Health:" + str(health)
+		isInvincible = true
+		invincibleTimer = invincibleTimerConstant
+	else: 
+		sprite.frame = 0
+	if blue:
+		blueMeter -= delta
+		meter.rect_size.x = maxMeterWidth*blueMeter/blueMeterMax
+	elif blueMeter < blueMeterMax:
+		blueMeter += delta
+		meter.rect_size.x = maxMeterWidth*blueMeter/blueMeterMax
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		position = event.position - Vector2(0, 16)
 	if event is InputEventMouseButton:
-		if event.pressed:
+		if event.pressed and blueMeter > 0:
 			sprite.modulate = Color(0.1,0.50,1)
 			blue = true
 		else:
@@ -32,13 +64,8 @@ func _input(event):
 
 func _on_body_shape_entered(_body_id, _body, _body_shape, _local_shape):
 	touching += 1
-	if touching >= 1: #and blue == false:
-		sprite.frame = 1
-		health = health - 1
-		print (health)
+
 
 
 func _on_body_shape_exited(_body_id, _body, _body_shape, _local_shape):
 	touching -= 1
-	if touching == 0: #or blue == true:
-		sprite.frame = 0
